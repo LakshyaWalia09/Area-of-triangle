45f0f1554228b3bad58c3239903585523020c6e6
LakshyaObviously <78919409+LakshyaWalia09@users.noreply.github.com>
2022-10-17 02:20:09 +0530

    Add files via upload

---
+++ 07-5-Subject-Avg-Marks.py
@@ -0,0 +1,7 @@
+A=int(input("Marks in maths: "))
+B=int(input("Marks in english: "))
+C=int(input("Marks in hindi: "))
+D=int(input("Marks in physics: "))
+E=int(input("Marks in chemistry: "))
+N=(A+B+C+D+E)/5
+print("Average score is",N,"Marks.")
